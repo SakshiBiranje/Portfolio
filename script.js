abcc31a1a07a71aac61820250b2273fced9664df
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-23 18:38:43 +0530

    Update

---
+++ script.js
@@ -0,0 +1,108 @@
+// Get all anchor links in the navbar
+var anchorLinks = document.querySelectorAll('.nav-links a');
+
+// Loop through each anchor link
+anchorLinks.forEach(function(anchor) {
+    // Add click event listener for each anchor link
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();  // Prevent default link behavior
+
+        var targetSection = document.querySelector(this.getAttribute('href')); // Get the target section
+        
+        // Scroll to the target section with smooth behavior
+        window.scrollTo({
+            top: targetSection.offsetTop - 60,  // Adjust scroll position (offset for fixed header, if needed)
+            behavior: 'smooth'  // Enable smooth scrolling
+        });
+    });
+});
+
+
+// Script File
+
+// Home Section Starts
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.nav-links');
+var menuLinks = document.querySelectorAll('.nav-links li a');
+
+menuBtn.addEventListener('click', activeClass);
+
+function activeClass(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+}
+
+for(i = 0; i < menuLinks.length; i++){
+	menuLinks[i].addEventListener('click', menuItemClicked);
+}
+
+function menuItemClicked(){
+	menuBtn.classList.remove('active');
+	menu.classList.remove('active');
+}
+
+var homeSection = document.querySelector('.home');
+window.addEventListener('scroll', scrollFunction);
+window.addEventListener('load', scrollFunction);
+
+function scrollFunction(){
+	if(window.scrollY > 60){
+		homeSection.classList.add('active');
+	}
+	else{
+		homeSection.classList.remove('active');
+	}
+}
+// Home Section Ends 
+
+// Portfolio Section Starts
+var $galleryContainer = $('.gallery').isotope({
+	itemSelector: '.item',
+	layoutMode: 'fitRows'
+})
+
+$('.button-group .button').on('click', function(){
+	$('.button-group .button').removeClass('active');
+	$(this).addClass('active');
+
+	var value = $(this).attr('data-filter');
+	$galleryContainer.isotope({
+		filter: value
+	})
+})
+
+// magnific popup
+$('.gallery').magnificPopup({
+	delegate: '.overlay a',
+	type: 'image',
+	gallery:{
+		enabled: true
+	}
+})
+// Portfolio Section Ends
+
+// Testimonials Section Starts
+$('.testimonials-container').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTime:6000,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+             "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:true
+        },
+        768:{
+            items:2
+        }
+    }
+	
+})
+
